487ca679fa507a01686d7a6f5120a32e3ff04803
Kartik Verma <kartikverma559@gmail.com>
2026-08-09 15:48:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Largest Zigzag Sequence/largest-zigzag-sequence.java
@@ -0,0 +1,55 @@
+class Solution {
+    int[][] dp;
+
+    public int zigzagSequence(int[][] mat) {
+
+        int n = mat.length;
+        int m = mat[0].length;
+
+        dp = new int[n][m];
+
+        for (int[] ele : dp) {
+            Arrays.fill(ele, -1);
+        }
+
+        int ans = 0;
+
+        // Try starting from every column of first row
+        for (int col = 0; col < m; col++) {
+            ans = Math.max(ans, solve(0, col, mat, n, m));
+        }
+
+        return ans;
+    }
+
+    private int solve(int r, int c, int[][] mat, int n, int m) {
+
+        // Base case: last row
+        if (r == n - 1) {
+            return mat[r][c];
+        }
+
+        // Already calculated
+        if (dp[r][c] != -1) {
+            return dp[r][c];
+        }
+
+        int best = 0;
+
+        // Try every column of next row
+        // except the current column
+        for (int nextCol = 0; nextCol < m; nextCol++) {
+
+            if (nextCol != c) {
+                best = Math.max(
+                    best,
+                    solve(r + 1, nextCol, mat, n, m)
+                );
+            }
+        }
+
+        dp[r][c] = mat[r][c] + best;
+
+        return dp[r][c];
+    }
+}
